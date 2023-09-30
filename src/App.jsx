bfdb1e732a0c9854526303641e7f66ed0d9eee6e
unknown <tyasweningas@gmail.com>
2023-09-30 22:41:58 +0700

    login

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,30 @@ import Register from './pages/Register'
 import Kategori from './pages/Kategori'
 import Home from './pages/Home'
 import Detail from './pages/Detail'
+import Login from './pages/Login' 
+import { BrowserRouter, Router } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <Header/>
+
+
+       <Header/>
       
       {/* <Register/> */}
 
-      {/* <Home/> */}
+      <Home/>
       
       {/* <Kategori/> */}
 
       {/* <Detail/> */}
       
-      <Footer/>
+      <Login/> 
+
+       <Footer/>
     </>
   )
 }
