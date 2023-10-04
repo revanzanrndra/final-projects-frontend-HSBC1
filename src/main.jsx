0c2911a599d4a0f572c0e0b218e486057da71f94
unknown <johndoegrandpa@gmail.com>
2023-10-04 17:28:05 +0700

    modifikasi url images

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "swiper/css";
 import "swiper/css/navigation";
 import "./index.css";
-import "./lib/language.js";
+// import "./lib/language.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
